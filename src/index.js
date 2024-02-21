e11b4fcd53272873ef97cd958186421e6b187293
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-02-21 14:54:32 +0100

    added more weather data using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 function updateWeather(response) {
   let updatedTempElement = document.querySelector("#temp");
   let temperature = response.data.temperature.current;
-  updatedTempElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#current-city");
+  let weatherDescription = document.querySelector("#description-weather");
+  let humidityCurrent = document.querySelector("#humidity");
+  let windSpeedCurrent = document.querySelector("#wind-speed");
+  let currentTime = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let minimumMaximum = document.querySelector("#minimum-maximum");
+
+  updatedTempElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityCurrent.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedCurrent.innerHTML = `${response.data.wind.speed} KM/H`;
+  currentTime.innerHTML = formatDate(date);
+  minimumMaximum.innerHTML = `${response.data.daily.temperature.minimum}/${response.data.daily.temperature.maximum}`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function submitCity(city) {
