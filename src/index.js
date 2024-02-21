5ecd6a5b67b7568ceb53ab12bc3d6d511f94ff72
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-02-21 13:16:06 +0100

    added functionality that was missing to previous work

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ function updateWeather(response) {
 function submitCity(city) {
   let apiKey = "2950072abb4303db56f019dto24c1aca";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(updateWeather);
 }
 
@@ -15,6 +14,7 @@ function actSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInput.value;
+  submitCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form-weather");
