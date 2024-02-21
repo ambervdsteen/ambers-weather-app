fda7b3e21e617863b254d0540612251abee1f8da
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-02-21 13:09:15 +0100

    changed axios script

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function updateWeather(response) {
 function submitCity(city) {
   let apiKey = "2950072abb4303db56f019dto24c1aca";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(updateWeather);
 }
 
