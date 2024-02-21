e3dcc1baf14401a37f26986b443aac1efbbd6cfc
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-02-21 11:36:16 +0100

    added api functionalities

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function updateWeather(response) {
+  let updatedTempElement = document.querySelector("#temp");
+  updatedTempElement.innerHTML = response.data.temperature.current;
+}
+
+function submitCity(city) {
+  let apiKey = "2950072abb4303db56f019dto24c1aca";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=&{apiKey}&units=metric";
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function actSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
